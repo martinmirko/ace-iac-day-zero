160a30d296124abf136a6d4e5467c0162fb0d748
martinmirko <80984229+martinmirko@users.noreply.github.com>
2024-01-11 14:14:30 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "martin-mirko"
+    organization = "martin-mirko"
     workspaces {
       name = "ace-iac-day-zero"
     }
