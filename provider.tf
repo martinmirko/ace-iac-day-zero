acc9f5a371514fb9f04184ed2d7913f417eda695
martinmirko <80984229+martinmirko@users.noreply.github.com>
2024-01-11 16:35:06 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,8 @@ provider "aviatrix" {
   controller_ip = var.controller_ip
   username      = var.username
   password      = var.password
+  skip_version_validation = true
+  #version                 = "2.19.0"
 }
 
 provider "aws" {
