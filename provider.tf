88064980e7311531da303f8f7582ead98a56b4bb
martinmirko <80984229+martinmirko@users.noreply.github.com>
2024-01-12 12:20:49 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,11 +11,11 @@ provider "aws" {
   region = var.aws_spoke1_region
 }
 
-#provider "azurerm" {
-#  features {}
-#  skip_provider_registration = "true"
-#  subscription_id            = var.azure_subscription_id
-#  client_id                  = var.azure_client_id
-#  client_secret              = var.azure_client_secret
-#  tenant_id                  = var.azure_tenant_id
-#}
+provider "azurerm" {
+  features {}
+  skip_provider_registration = "true"
+  subscription_id            = var.azure_subscription_id
+  client_id                  = var.azure_client_id
+  client_secret              = var.azure_client_secret
+  tenant_id                  = var.azure_tenant_id
+}
